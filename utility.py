a6692c9714e9e77a52b087ca146525413deeb6d1
Mac Guise <aguise@umich.edu>
2019-07-02 08:52:42 -0400

    Adjusts multi-line comment format

--- utility.py
+++ utility.py
@@ -1,8 +1,8 @@
 import fuzzywuzzy as fuzz
 
-''' Simple fucntion for calculating
-ration of similarity between two strings
-by using their levenshstein distance '''
+# Simple fucntion for calculating
+# ration of similarity between two strings
+# by using their levenshstein distance
 
 def Ratio (a, b) {
 	ratio = fuzz.ratio(a.lower(), b.lower())
