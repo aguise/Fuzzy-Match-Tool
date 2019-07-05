ffa61af97db858570094db124ff49114338e7c70
aguise <andrew.guise@bnymellon.com>
2019-07-05 14:14:38 -0400

    Adds comments

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import argparse
 import sys
 import csv
 import utility
+import openpyxl
 
 def main():
     for i in sys.argv:
+
+        # reads csv files from command line
         readCSV = csv.reader(sys.argv, delimiter=',')
         print(readCSV)
         for row in readCSV:
             for column in readCSV:
+
+                # prints the ratio of similarity for each word in csv file
                 print(utility.Ratio(readCSV, "usd"))
     return
 
