31fbea903b2f2b645840aa2c348c2ade3b0d7752
aguise <aguise@umich.edu>
2019-07-11 09:50:38 -0400

    adjusts raw data

--- main.py
+++ main.py
@@ -5,18 +5,7 @@ import utility
 import openpyxl
 
 def main():
-
-    rows = []
-
-    # reads csv files from command line
-    readCSV = csv.reader(sys.argv, delimiter=',')
-    print(readCSV)
-    for row in readCSV:
-        rows.append(row)
-        
-    for row in rows:
-        for col in row:
-            print(col)
-return
+    utility.CSV(sys.argv[1])
+    return
 
 main()
