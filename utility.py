04713628ebe2b67d34957186c222ed784b18c41b
aguise <aguise@umich.edu>
2019-07-11 09:50:38 -0400

    adjusts raw data

--- utility.py
+++ utility.py
@@ -1,4 +1,5 @@
 from fuzzywuzzy import fuzz
+import csv
 
 # Simple fucntion for calculating
 # ration of similarity between two strings
@@ -6,3 +7,14 @@ from fuzzywuzzy import fuzz
 
 def Ratio (a, b) :
 	return fuzz.ratio(a.lower(), b.lower())
+
+def CSV (file):
+	  
+    # reads csv files from command line
+    with open(file, 'r') as csvFile:
+        reader = csv.reader(csvFile)
+
+        for row in reader:
+            print(row)
+    
+    return
