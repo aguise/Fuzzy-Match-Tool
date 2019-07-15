3aac35269dd59729b8ae7b72db18096a3a47dc59
aguise <aguise@umich.edu>
2019-07-15 16:58:38 -0400

    creates base functionality

---
+++ dictionary.py
@@ -0,0 +1,43 @@
+
+def currency():
+    dictionary = {
+        "united states of america" : "usd",
+        "united states" : "usd",
+        "america" : "usd",
+        "canadian" : "cad",
+        "swiss franc": "chf",
+        "australian" : "aud",
+        "japanese yen" : "jpy",
+        "new zealand" : "nzd",
+        "euro" : "eur",
+        "pound" : "gbp",
+        "swedish krona" : "sek",
+        "danish krone" : "dkk",
+        "krona norwegia" : "nok",
+        "singapore" : "sgd",
+        "czech koruna" : "czk",
+        "hong kong" : "hkd",
+        "mexican" : "mxn",
+        "zloty" : "pln",
+        "ruble" : "rub",
+        "turkish lira" : "try",
+        "rand" : "zar",
+        "yuan" : "cnh",
+        "united arab emirates dirham" : "aed",
+        "afghanistan afghani" : "afn",
+        "albania lek" : "all",
+        "armenia dram" : "amd",
+        "netherlands antilles guilder" : "ang",
+        "angola kwanza" : "aoa",
+        "argentina" : "ars"
+    }
+
+    return dictionary
+
+def country():
+    dictionary = {
+        "united states" : "us",
+        "canada" : "ca"
+    }
+
+    return dictionary
