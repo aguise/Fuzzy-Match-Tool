adebb82803cdf5270e699d2ed1909b99ef65efb2
aguise <aguise@umich.edu>
2019-07-18 17:14:05 -0400

    adds files for functioning match tool

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ import argparse
 import sys
 import csv
 import utility
-import openpyxl
+import xlwt
+from xlwt import Workbook
 
 def main():
 
-    dict = utility.initialize(sys.argv[1])
+    dict_type = sys.argv[1]
+    dict = utility.initialize(dict_type)
     standard_list = []
     whitelist = set('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
 
     file = sys.argv[2]
+    file_standards = sys.argv[3]
 
     # reads csv files from command line
-    with open(file, 'r') as csvFile:
-        reader = csv.reader(csvFile)
-    
-        for row in reader:
+    with open(file, 'r') as csv_file, open(file_standards, 'r') as csv_file_2:
+        reader = csv.reader(csv_file)
+        reader_2 = csv.reader(csv_file_2)
+        
+        for row, row_2 in zip(reader, reader_2):
             for i in row:
                 string = ''.join(filter(whitelist.__contains__, i))
-                utility.match(string, dict)
-        
+                standard_list.append(utility.match(string.lower(), dict, dict_type))
 
+        utility.final_output(row, row_2, standard_list)
+        
 main()
